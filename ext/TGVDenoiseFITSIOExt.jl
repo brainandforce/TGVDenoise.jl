665aebcc527e0f11557e6104db790ddcc411b46f
Brandon Flores <bsflores@wisc.edu>
2025-11-17 23:26:47 -0600

    Added utility methods for denoising FITS files

--- ext/TGVDenoiseFITSIOExt.jl
+++ ext/TGVDenoiseFITSIOExt.jl
@@ -3,6 +3,69 @@ module TGVDenoiseFITSIOExt
 using TGVDenoise
 using FITSIO
 
+using Base.Filesystem: cp
 
+function tgv_denoise_channels!(
+    hdu::ImageHDU,
+    alpha,
+    beta;
+    kwargs...
+)
+    denoised = TGVDenoise.tgv_denoise_channels(read(hdu), alpha, beta; kwargs...)
+    write(denoised, hdu)
+    return hdu
+end
+
+function tgv_denoise_channels!(
+    fits::FITS,
+    alpha,
+    beta;
+    hdu_index = 1,
+    kwargs...
+)
+    hdu = fits[hdu_index]
+    tgv_denoise_channels!(hdu, alpha, beta, kwargs...)
+end
+
+"""
+    tgv_denoise_channels(
+        fits::FITS,
+        alpha,
+        beta;
+        prefix = "tgv",
+        hdu_index = 1,
+        force = false,
+        kwargs...
+    )
+
+Performs total TGV denoising independently on each color channel on the selected header data unit
+(HDU) of a FITS file.
+By default, the first HDU is chosen, but this can be changed with the `hdu_index` keyword.
+
+The original file is copied to a file appended with a prefix given by the `prefix` parameter plus
+an underscore.
+If the file already exists, it will not be overwritten unless `force` is set to `true`
+"""
+function TGVDenoise.tgv_denoise_channels(
+    fits::FITS,
+    alpha,
+    beta;
+    prefix = "tgv",
+    hdu_index = 1,
+    force = false,
+    kwargs...
+)
+    # Create a copy of the target file
+    new_filename = let 
+        sf = splitdir(fits.filename)
+        joinpath(first(sf), string(ifelse(isnothing(prefix), "", prefix)) * '_' * last(sf))
+    end
+    cp(fits.filename, new_filename; force)
+    FITS(new_filename, "r+") do new_fits
+        tgv_denoise_channels!(new_fits[hdu_index], alpha, beta; kwargs...)
+    end
+end
+
+export tgv_denoise_channels!
 
 end
